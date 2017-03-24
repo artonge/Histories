b96ca08ed09c33b1458360d794c0f8e050bc0b87
Louis Chemineau <artonge.c@gmail.com>
2017-03-24 19:25:21 +0100

    Save

---
+++ src/Services/PointsFactory.ts
@@ -0,0 +1,20 @@
+export default class PointsFactory {
+
+  private pointsCount: number
+
+  constructor(userID: string) {
+    this.pointsCount = 0
+  }
+
+  getPoints() {
+    return this.pointsCount
+  }
+
+  add(points: number) {
+    this.pointsCount += points
+  }
+
+  remove(points: number) {
+    this.pointsCount -= points
+  }
+}
